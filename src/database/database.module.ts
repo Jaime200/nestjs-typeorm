e70919b4924c0423e20d3f82a2896c4a607ba123
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-05-15 10:11:00 -0600

    Se utiliza useFactory, configuracion de importacion de config como una inyeccion y tipado con ConfigType

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,30 +1,13 @@
 import { Module, Global } from '@nestjs/common';
 import * as sql  from 'mssql'
+import configAPP from '../config'
 import { ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm'
+
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 
-const config :sql.config = {
-  user: 'sa',
-  port: 1433,
-    password: '12345',
-    server: 'DESKTOP-CVIG606\\SQLEXPRESS2019',
-    database: 'platziST',
-    options: {
-      trustServerCertificate:true
-    }
-}
-const client = new sql.ConnectionPool(config)
-
-// client.connect().then(async  c=>{
-//   console.log('Conectado')
-//   const result = await c.request().query("SELECT * FROM [dbo].[tasks]")
-//   console.log(result);
-// }).catch(err=>{
-//   console.error(err.message)
-// });
 
 
 @Global()
@@ -34,10 +17,29 @@ const client = new sql.ConnectionPool(config)
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    // {
+    //   provide : 'SQL',
+    //   useValue: client
+    // }
     {
-      provide : 'SQL',
-      useValue: client
-    }
+      provide:'SQL',
+      useFactory: (configService: ConfigType<typeof configAPP>) =>{
+        const { name, port, password, server, user } = configService.database
+        const config :sql.config = {
+          user: user,
+          port: port,
+          password:password,
+          server: server,
+          database: name,
+            options: {
+              trustServerCertificate:true
+            }
+        }
+        return new sql.ConnectionPool(config)
+      },
+      inject : [configAPP.KEY]
+    },
+    
   ],
   exports: ['API_KEY', 'SQL'],
 })
