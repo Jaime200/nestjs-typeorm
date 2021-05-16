53453b2aa9cb3b71bb615e978143502bbb7f62a1
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-05-16 12:05:34 -0600

    Parametrizando entidades de typeorm

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,4 +1,15 @@
+
+import { 
+  PrimaryGeneratedColumn 
+  ,Column
+  ,Entity
+} from 'typeorm'
+
+@Entity()
 export class Category {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type:'varchar', length:255, unique:true })
   name: string;
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,27 @@
+import { 
+  PrimaryGeneratedColumn 
+  ,Column
+  ,Entity
+} from 'typeorm'
+
+@Entity()
 export class Product {
+
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type:'varchar', length: 255, unique:true })
   name: string;
+
+  @Column({ type:'text'})
   description: string;
+
+  @Column({ type:'money'})
   price: number;
+
+  @Column({ type:'money'})
   stock: number;
+
+  @Column({ type:'varchar'})
   image: string;
 }
